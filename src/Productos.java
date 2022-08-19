c92d9b60932716be0fd349af6ae588bace181023
viviana2224 <mticpro44fi_bog@unal.edu.co>
2022-08-18 20:15:19 -0500

    v3-Completamos Productos

--- src/Productos.java
+++ src/Productos.java
@@ -1,4 +1,13 @@
 
 public class Productos {
     
+    private String nombre;
+    private int precio;
+    private String categoria;
+
+    public Productos(String nombre, int precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
 }
